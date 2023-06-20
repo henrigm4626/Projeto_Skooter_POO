3f665d24882f9e34886f3b532738a0baa80a2d1f
Henrique Marques <88845710+henrigm4626@users.noreply.github.com>
2023-06-20 02:57:46 -0300

    Alterando o nome das imagens

--- Skooter/src/Modelo/CriaBlocos.java
+++ Skooter/src/Modelo/CriaBlocos.java
@@ -4,13 +4,13 @@ public class CriaBlocos {
     
     public static Blocos criaBloco(String tipo){
         if(tipo.equalsIgnoreCase("verde")){
-            return new BlocoVerdeEstatico("blocoVerde.png");
+            return new BlocoVerdeEstatico("bloco_verde.png");
         } else if(tipo.equals("verdeMovel")){
-            return new BlocoVerdeMovel("blocoVerdeMovel.png");
+            return new BlocoVerdeMovel("bloco_verde_movel.png");
         } else if(tipo.equals("vermelho")){
-            return new BlocoVermelhoEstatico("blocoVermelho.png");
+            return new BlocoVermelhoEstatico("bloco_vermelho.png");
         } else if(tipo.equals("vermelhoMovel")){
-            return new BlocoVermelhoMovel("blocoVermelhoMovel.png");
+            return new BlocoVermelhoMovel("bloco_vermelho_movel.png");
         } else{
             return null;
         }
